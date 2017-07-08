63733bbd4e7fabac0dfb260b3caaba6ddd2e8351
yukonboy <yukonboy@telus.net>
2017-07-08 17:33:45 +0000

    Font Changes - Lesson 3.10

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,18 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Splurgy
-  class Application < Rails::Application
+  class Application < Rails:Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+  
   end
 end
+
+
